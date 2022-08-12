4d8eee4f5d453fce836da1896dd1f77e1d44654a
prifiyia.n <prifiyia.n@enjayworld.com>
2022-08-12 23:39:37 +0530

    api service changes

--- MVVMDemo/Service/APIService.swift
+++ MVVMDemo/Service/APIService.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+class APIService {
+    func callAPI(url: String, completion: @escaping (_ data: Data?, _ status: Bool) -> ()) {
+        URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { [weak self] (data, response, error) in
+            if error != nil {
+                if let data = data {
+                    completion(data, true)
+                }
+            }else {
+                completion(data, false)
+            }
+        }).resume()
+    }
+}
