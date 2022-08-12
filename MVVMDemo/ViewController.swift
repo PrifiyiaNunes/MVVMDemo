531812c045df2f92a74b62aa68f96a47d1de5a69
prifiyia.n <prifiyia.n@enjayworld.com>
2022-08-12 22:58:37 +0530

    Initial Commit

---
+++ MVVMDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMDemo
+//
+//  Created by Prifiyia on 12/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
