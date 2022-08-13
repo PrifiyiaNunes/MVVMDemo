1dbe36ab26c9ddf0a2e04e716cbb4973b566420a
prifiyia.n <prifiyia.n@enjayworld.com>
2022-08-13 15:17:39 +0530

    model and apiservice

--- MVVMDemo/Service/APIService.swift
+++ MVVMDemo/Service/APIService.swift
@@ -8,6 +8,17 @@
 import Foundation
 
 class APIService {
+    
+    private static var sharedInstance : APIService = {
+        return APIService()
+    }()
+    
+    class func shared() -> APIService {
+        return sharedInstance
+    }
+    
+    private init() {}
+    
     func callAPI(url: String, completion: @escaping (_ data: Data?, _ status: Bool) -> ()) {
         URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { [weak self] (data, response, error) in
             if error != nil {
--- MVVMDemo/ViewModel/QuestionViewModel.swift
+++ MVVMDemo/ViewModel/QuestionViewModel.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+class QuestionViewModel {
+    private var url = "https://quiz-68112-default-rtdb.firebaseio.com/quiz.json"
+    var quesionModel : QuestionModel?
+    
+    private func callApi() {
+        APIService.shared().callApi(url: url, completion: { [weak self] (data, status) in
+            if let data = data {
+                let jsonDecoder = JSONDecoder()
+                let finalModel = try! jsonDecoder.decode(QuestionModel.self, from: data)
+                self.quesionModel = finalModel
+            }
+        })
+    }
+}
